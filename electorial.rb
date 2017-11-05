41c3d1b1f8d42d91f9b2ae9fb6ccda1fffc773e2
carolyn arnold <carolyn.d.arnoldlp@gmail.com>
2017-11-05 22:22:02 +0000

    Corrected spacing and syntx before testing

--- electorial.rb
+++ electorial.rb
@@ -11,109 +11,96 @@ puts "Voter Simulation gives you the ability to create, list, update, and delete
 puts "Options: (C)reate, (L)ist, (U)pdate, (D)elete or (E)nd records?"
 case option = gets.chomp.upcase
       
-    when "C"
-       puts "Would you like to create? options: (V)oter or (P)olitician"
-    case option_create = gets.chomp.upcase
+  when "C"
+    puts "Would you like to create? options: (V)oter or (P)olitician"
+      case option_create = gets.chomp.upcase
     
-    when "V"
-     puts "Name (First and Last)?"
-     case name = gets.chomp.upcase
-     puts "Political Affiliation? options: (L)iberal, (C)onservative, (T)ea Party, (S)ocialist, or (N)eutral"
-     case option_politics = gets.chomp.upcase
+     when "V"
+       puts "Name (First and Last)?"
+         case name = gets.chomp.upcase
+           puts "Political Affiliation? options: (L)iberal, (C)onservative, (T)ea Party, (S)ocialist, or (N)eutral"
+         case option_politics = gets.chomp.upcase
          
-     when "L"
-       records.create_voter(name, "Liberal")
-       puts "New voter record for [#{name}, Liberal] created."
+      when "L"
+        records.create_voter(name, "Liberal")
+          puts "New voter record for [#{name}, Liberal] created."
       when "C"
-       records.create_voter(name, "Conservative")
-       puts "New voter record for [#{name}, Conservative] created."
+        records.create_voter(name, "Conservative")
+          puts "New voter record for [#{name}, Conservative] created."
       when "T"
-       records.create_voter(name, "Tea Party")
-       puts "New voter record for [#{name}, Tea Party] created."
+        records.create_voter(name, "Tea Party")
+          puts "New voter record for [#{name}, Tea Party] created."
       when "S"
-       records.create_voter(name, "Socialist")
-       puts "New voter record for [#{name}, Socialist] created."
+        records.create_voter(name, "Socialist")
+          puts "New voter record for [#{name}, Socialist] created."
       when "N"
-       records.create_voter(name, "Neutral")
-       puts "New voter record for [#{name}, Neutral] created."
+        records.create_voter(name, "Neutral")
+          puts "New voter record for [#{name}, Neutral] created."
       else
-       puts "Oops! That options is not available. Let's try again."
-     end
+        puts "Oops! That options is not available. Let's try again."
+      end
     
-    when "P"
-      puts "Name (First, Last)?"
-      case name = gets.chomp.upcase
-      puts "Party_Affiliation? options: (D)emocrat or (R)epublican"
-      case party_affiliation = gets.chomp.upcase
+     when "P"
+       puts "Name (First, Last)?"
+         case name = gets.chomp.upcase
+       puts "Party_Affiliation? options: (D)emocrat or (R)epublican"
+         case party_affiliation = gets.chomp.upcase
     
-    when "D"
-       records.create_politician(name, "Democrat")
-       puts "New party record for [#{name}, Democrat] created."
-    when "R"
-       records.create_politician(name, "Republican")
-       puts "New party record for [#{name}, Republican] created."
-    else
-       puts "Oops! That options is not available. Let's try again."
-    end
-end           
+      when "D"
+        records.create_politician(name, "Democrat")
+        puts "New party record for [#{name}, Democrat] created."
+      when "R"
+        records.create_politician(name, "Republican")
+        puts "New party record for [#{name}, Republican] created."
+      else
+        puts "Oops! That options is not available. Let's try again."
+      end
+   end           
     
-    when "L"
-     puts records.list_voters()
-     puts records.list_politicians()
+  when "L"
+    puts records.list_voters()
+    puts records.list_politicians()
 
-    when "U"
-      puts "Who would you like to update? options: Name (First, Last)"
-        case option_update_name = gets.chomp.upcase
-      person_update = records.find_person_by_name(update_name)    
-        if person_update == nil
-           puts "#{update_name} not found."
-    else
-      puts "Updated Name"
-        case new_name = gets.chomp.upcase
-      person_update.update_new(new_name)
+  when "U"
+    puts "Who would you like to update? options: Name (First, Last)"
+      case option_update_name = gets.chomp.upcase
+    person_update = records.find_person_by_name(update_name)    
+      if person_update == nil
+        puts "#{update_name} not found."
+      else
+        puts "Updated Name"
+          case new_name = gets.chomp.upcase
+            person_update.update_new(new_name)
       
-   if person_update.is_a?(Voter)
-      puts "Are updating your political affiliation options: (L)iberal,(C)onservative, (T)ea Party, (S)ocialist, or (N)eutral"
-   case new_politics = gets.chomp.upcase
-      person_update.update_politics([options: "Liberal", "Conservative", "Tea Party", "Socialist", "Neutral"].detect{ |politics| politics.start_with?(new_politics) })
+      if person_update.is_a?(Voter)
+        puts "Are updating your political affiliation options: (L)iberal,(C)onservative, (T)ea Party, (S)ocialist, or (N)eutral"
+          case new_politics = gets.chomp.upcase
+            person_update.update_politics([options: "Liberal", "Conservative", "Tea Party", "Socialist", "Neutral"].detect{ |politics| politics.start_with?(new_politics)})
       
-   else
-      puts "Are you updating your party affiliation? options: (D)emocrat or (R)epublican"
-   case new_party_affiliation = gets.chomp.upcase
-      person_update.update_party_affiliation([options: "Democrat", "Republican"].detect{ |party_affiliation| party_affiliation.start_with?(new_party_affiliation) })
-   end
-end
-
-    when "D"
-       party_affiliation = "Democrat"
-       @records.update_politician(old_name, new_name, new_party_affiliation)
-    when "C"
-       party_affiliation = "Republican"
-       @records.update_politician(old_name, new_name, new_party_affiliation)
-    else
-       puts "Oops! That options is not available. Let's try again."
+      else
+        puts "Are you updating your party affiliation? options: (D)emocrat or (R)epublican"
+          case new_party_affiliation = gets.chomp.upcase
+            person_update.update_party_affiliation([options: "Democrat", "Republican"].detect{ |party_affiliation| party_affiliation.start_with?(new_party_affiliation)})
     end
-  end
-end  
-      
+  end 
       
   when "D"
     puts "Who would you like to delete? option: name (first, last)"
-    case option_name_delete= gets.chomp.upcase
-    person_delete = records.find_person_by_name(delete_name)
-    if person_delete == nil
-    puts "#{delete_name} not found."
-    else
-      records.delete_person(person_delete)
-      puts "#{delete_name} removed."
-    end
+      case option_name_delete= gets.chomp.upcase
+        person_delete = records.find_person_by_name(delete_name)
+      if person_delete == nil
+        puts "#{delete_name} not found."
+      else
+        records.delete_person(person_delete)
+        puts "#{delete_name} removed."
+     end
         
-    else
-      puts "Oops! That options is not available. Let's try again."
-    end
-            
- when "E"
-      puts "You are now leaving Voter Simulation...Goodbye!"
+      else
+        puts "Oops! That options is not available. Let's try again."
+     end
+  end      
+  when "E"
+     puts "You are now leaving Voter Simulation...Goodbye!"
     end
   end
 end
